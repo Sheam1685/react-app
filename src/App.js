7c6d0928acd3a9cfacc06daeb143cdcd09568183
Sheam1685 <samia12676@gmail.com>
2023-06-01 16:17:14 +0600

    stateful tasks with global state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import { useState } from 'react';
+
+const initialTasks = [  
+  {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: '2023-02-05',
+      reminder: true,
+  },
+  {
+      id: 2,
+      text: 'Meeting at School',
+      day: '2023-02-06',
+      reminder: true,
+  },
+  {
+      id: 3,
+      text: 'Food Shopping',
+      day: '2023-02-07',
+      reminder: false,
+  },
+];
 
 function App() {
+  const [tasks, setTasks] = useState(initialTasks);
   return (
     <div className="container">
       <Header title='TASK TRACKER'/>
-      <Tasks />
+      {/* send tasks and setTasks as parameter */}
+      <Tasks tasks={tasks} setTasks={setTasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,34 +1,31 @@
 import React from 'react'
 
-const tasks = [  
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    },
-]
-
-const Tasks = () => {
-  return (
+const Tasks = ({ tasks, setTasks }) => {
+    const onClick = (id) => {
+        setTasks(prevTasks => {
+            return prevTasks.map((task) => {
+                if (task.id === id) {
+                    const d = new Date(task.day);
+                    d.setDate(d.getDate() + 1);
+                    return {...task, day : d.toISOString().slice(0,10)}
+                }
+                return task;
+            })
+        });
+    }
+    return (
     <>
         {tasks.map((task) => (
-            <h3 key={task.id}>{task.text}</h3> // key is needed for each item in the list. It is needed for react to know which item is which
+            // {/* key is needed for each item in the list. It is needed for react to know which item is which */}
+            <div className='task' key={task.id}> 
+            <h3 style={{width:'33%'}}>{task.text}</h3>
+            <code>{task.day}</code>
+            {/* send task id as parameter */}
+            <button onClick={() => onClick(task.id)} className='btn'>Delay</button>
+            </div>
         ))}
     </>
-  )
+    )
 }
 
-export default Tasks
+export default Tasks;
